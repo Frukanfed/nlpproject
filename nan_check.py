ca6c620d81875015f8316731ab2d73827d30191b
frukanfed <frukansalih@gmail.com>
2024-05-19 01:39:29 +0300

    update

---
+++ nan_check.py
@@ -0,0 +1,20 @@
+import pandas as pd
+
+# CSV dosyasını yükleyin
+df = pd.read_csv("./data/processed_data.csv")
+
+# NaN verileri kontrol edin
+nan_var_mi = df.isnull().values.any()
+print(f"NaN verisi var mı: {nan_var_mi}")
+
+# NaN verilerin sayısını sütun bazında kontrol edin
+print("Her sütundaki NaN verilerin sayısı:")
+print(df.isnull().sum())
+
+# NaN satırlarını silin
+df_temiz = df.dropna()
+
+# Temizlenmiş veri setini yeniden kaydedin
+df_temiz.to_csv("./data/processed_nachecked_data.csv", index=False)
+
+print("NaN veriler temizlendi ve dosya yeniden kaydedildi.")
